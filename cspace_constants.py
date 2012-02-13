4d48c24c87dd96aade82050c634f2f59a04e7747
Chris Thompson <chris.thompson@walkerart.org>
2012-02-13 11:08:16 -0600

    change constants

--- cspace_constants.py
+++ cspace_constants.py
@@ -1,5 +1,4 @@
-CSPACE_URL = 'http://your_server:your_port/cspace-services/'
-CSPACE_USER = 'username@collectionspace.org'
-CSPACE_PASS = 'password'
+CSPACE_URL = 'http://ec2-23-20-45-108.compute-1.amazonaws.com:8180/cspace-services/'
+CSPACE_USER = 'admin@walkerart.org'
 CS_OBJECT_FILE = 'collectionspace_objects.pickle'
-OAI_URL = 'http://example.org/oai/'
+OAI_URL = 'http://collections.walkerart.org/oai/'
